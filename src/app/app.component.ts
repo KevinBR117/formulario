5d033993ac15de8e2041773f9211d2c4946e12c4
KevinBR117 <kevinbarbozarodriguez117@gmail.com>
2023-02-28 12:55:07 -0600

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  registerForm !:FormGroup
   title = 'formulario';
+  submitted=false;
+  constructor(private formBuilder: FormBuilder){
+  }
+  ngOnInit(){
+    this.registerForm=this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+  onSubmit(){
+    this.submitted=true;
+    //detiene el proceso si la forma es invalida
+    if(this.registerForm.invalid){
+      return ;
+    }else{
+      alert("SUCCESS")
+    }
+  }
+
 }
